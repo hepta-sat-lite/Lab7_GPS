4dabe04d9cdf7250a7a46383c52dfac183dc5b99
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-10 12:59:24 +0000

    commit610

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "mbed.h"
+#include "HEPTA_CDH.h"
+#include "HEPTA_EPS.h"
+#include "HeptaCamera_GPS.h"
 
-DigitalOut myled(LED1);
+HEPTA_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd");
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(PA_0,PA_4);
+HeptaCamera_GPS sensor(PA_9,PA_10);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main()
+{
+    sensor.gps_setting();
+    int quality=0,stnum=0,gps_check=0;
+    char ns='A',ew='B',aunit='m';
+    float time=0.0,latitude=0.0,longitude=0.0,hacu=0.0,altitude=0.0;
+    for(int i=1; i<1000; i++) {
+        sensor.gga_sensing(&time, &latitude, &ns, &longitude, &ew, &quality, &stnum, &hacu, &altitude, &aunit, &gps_check);
+        if((gps_check==0)|(gps_check==1)) {
+            pc.printf("GPGGA,%f,%f,%c,%f,%c,%d,%d,%f,%f,%c\r\n",time,latitude,ns,longitude,ew,quality,stnum,hacu,altitude,aunit);
+        }
+    }
 }
